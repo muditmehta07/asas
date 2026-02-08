12db2ecbc14ecc9fc492343171cf40386d2810f1
muditmehta07 <muditmehta27@outlook.com>
2026-02-09 00:45:12 +0530

    dashboard update

---
+++ dashboard/src/components/Map.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Map = () => {
+    return (
+        <div style={{ width: '100%', height: '100%', backgroundColor: '#e0e0e0', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <h3>failed to fetch pose.</h3>
+        </div>
+    );
+};
+
+export default Map;
---
+++ ros_ws/webvideo.sh
@@ -0,0 +1,2 @@
+source /opt/ros/humble/setup.bash
+ros2 run web_video_server web_video_server
